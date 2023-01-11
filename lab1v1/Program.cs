bb8f729f21c5bdb35d2d7dff785ab3acd25b5d4d
Alex Lipinsky <jambo.alexxx@gmail.com>
2023-01-11 15:29:31 +0300

    Zad1

---
+++ lab1v1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());//запросить переменную а
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());//запросить переменную b
+
+            Console.WriteLine("{0} + {1} = {1} + {0}", a, b);//вывести результат
+
+        }
+    }
+}
